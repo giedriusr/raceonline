df1c1381f441a86a7194dbed825a83161b25e5d2
Giedrius Rimkus <info@giedriusr.lt>
2013-06-29 22:11:40 +0300

    initial commit for raceonline.lt ver.2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raceonline::Application.config.secret_key_base = '69028c414c971aca662052cbe81f7dbdd5e74d47dbf53f76a2f498fcfbac80b229f45a236b199d4a791223d26d7cb724ac11ec88440d912e6c6b970e004f4680'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raceonline::Application.config.session_store :cookie_store, key: '_raceonline_session'

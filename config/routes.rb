f9a28570e7864f7923992bb3bb9eeff0e4906c4c
Giedrius Rimkus <info@giedriusr.lt>
2013-07-10 13:29:50 +0300

    Generating Devise user views. adding nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Raceonline::Application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    delete '/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+    get '/sign_in', to: 'devise/sessions#new', as: 'sign_in'
+    get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
+    get '/reset_password', to: 'devise/passwords#new', as: 'reset_password'
+    get '/user/account', to: 'devise/registrations#edit', as: 'account'
+  end
 
   root 'home#index'
 end
